6a2aa064edd32eddf1ffc28d3c465d90682b2d62
yueXiao2 <57817476+yueXiao2@users.noreply.github.com>
2021-06-02 10:59:30 +1000

    Update CNN_Model.py

--- CNN_Model.py
+++ CNN_Model.py
@@ -6,7 +6,7 @@ import numpy as np
 from pre_process import *
 
 class ConvNet_RGB(nn.Module):
-    def __init__(self, contain_linear = False, filter_num = 14, kernel_size = (2,3), channels = 3):
+    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3), channels = 3):
         super(ConvNet_RGB, self).__init__()
         self.flatten = nn.Flatten()
         self.conv1 = nn.Conv2d(channels, filter_num, kernel_size = (2,3))
@@ -39,10 +39,46 @@ class ConvNet_RGB(nn.Module):
       if inspect_feature:
         return first_layer,conv_x,output_x
       return output_x
+    
+    
+class ConvNet_MultiChannel(nn.Module):
+    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3), channels = 8):
+        super(ConvNet_MultiChannel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.conv1 = nn.Conv2d(channels, filter_num, kernel_size = (2,3))
+        self.conv2 = nn.Sequential(nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
+            nn.Dropout(0.25),
+            nn.Conv2d(filter_num, 24, kernel_size = (2,3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
+        )
+
+        self.contain_linar = contain_linear
+
+        if contain_linear:
+          self.linear = nn.Sequential(
+              nn.Linear(24*6*2, 1024),
+              nn.Linear(1024, 5),
+          )
+
+    def forward(self, x, inspect_feature = False):
+
+      first_layer = self.conv1(x)
+      conv_x = self.conv2(first_layer)
+
+      output_x = conv_x
+      if self.contain_linar:
+        conv_x_flat  = self.flatten(conv_x)
+        output_x = self.linear(conv_x_flat)
+      
+      if inspect_feature:
+        return first_layer,conv_x,output_x
+      return output_x
 
 
 class ConvNet(nn.Module):
-    def __init__(self, contain_linear = False, filter_num = 14, kernel_size = (2,3)):
+    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3)):
         super(ConvNet, self).__init__()
         self.flatten = nn.Flatten()
         self.conv1 = nn.Conv2d(1, filter_num, kernel_size = (2,3))
