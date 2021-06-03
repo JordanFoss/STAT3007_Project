41fbede4cd35603afb6e1da2dc39cdd79b375f77
yueXiao2 <57817476+yueXiao2@users.noreply.github.com>
2021-06-03 14:00:38 +1000

    Update Models.py

--- Models.py
+++ Models.py
@@ -1,96 +1,53 @@
 import torch.nn as nn
 import torch
+import math 
 
-class ConvNet_RGB(nn.Module):
-    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3), channels = 3):
-        super(ConvNet_RGB, self).__init__()
-        self.flatten = nn.Flatten()
-        self.conv1 = nn.Conv2d(channels, filter_num, kernel_size = (2,3))
-        self.conv2 = nn.Sequential(nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
-            nn.Dropout(0.25),
-            nn.Conv2d(filter_num, 24, kernel_size = (2,3)),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
-        )
-
-        self.contain_linar = contain_linear
-
-        if contain_linear:
-          self.linear = nn.Sequential(
-              nn.Linear(24*32*14, 1024),
-              nn.Linear(1024, 5),
-          )
-
-    def forward(self, x, inspect_feature = False):
-
-      first_layer = self.conv1(x)
-      conv_x = self.conv2(first_layer)
-
-      output_x = conv_x
-      if self.contain_linar:
-        conv_x_flat  = self.flatten(conv_x)
-        output_x = self.linear(conv_x_flat)
-      
-      if inspect_feature:
-        return first_layer,conv_x,output_x
-      return output_x
+def output_size(input_shape, kernel_size,padding = 0, dilation = 1, stride = 1):
+    output_shape = (input_shape + (2 * padding) - dilation * (kernel_size - 1)-1)/stride
     
+    output_shape = math.floor(output_shape + 1)
     
-class ConvNet_MultiChannel(nn.Module):
-    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3), channels = 8):
-        super(ConvNet_MultiChannel, self).__init__()
-        self.flatten = nn.Flatten()
-        self.conv1 = nn.Conv2d(channels, filter_num, kernel_size = (2,3))
-        self.conv2 = nn.Sequential(nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
-            nn.Dropout(0.25),
-            nn.Conv2d(filter_num, 24, kernel_size = (2,3)),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
-        )
-
-        self.contain_linar = contain_linear
-
-        if contain_linear:
-          self.linear = nn.Sequential(
-              nn.Linear(24*6*2, 1024),
-              nn.Linear(1024, 5),
-          )
-
-    def forward(self, x, inspect_feature = False):
-
-      first_layer = self.conv1(x)
-      conv_x = self.conv2(first_layer)
-
-      output_x = conv_x
-      if self.contain_linar:
-        conv_x_flat  = self.flatten(conv_x)
-        output_x = self.linear(conv_x_flat)
-      
-      if inspect_feature:
-        return first_layer,conv_x,output_x
-      return output_x
-
-
+    return output_shape
+    
+def CNN_output_shape(input_dimension = (128,63),
+                     kernel = (2,3),
+                     layers = 2,
+                     max_pooling = True,
+                     max_kernel = (1,2)):
+    
+    row, column = input_dimension
+    for i in range(layers):
+        row = output_size(row, kernel[0])
+        column = output_size(column, kernel[1])
+        
+        row = output_size(row,max_kernel[0],stride = 2)
+        column = output_size(column, max_kernel[1], stride = 2)
+        
+    return row, column
+   
 class ConvNet(nn.Module):
-    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3)):
+    def __init__(self, contain_linear = False, filter_num = 8, kernel_size = (2,3), input_shape = (1,128,63)):
         super(ConvNet, self).__init__()
+        
+        channels,freq,times = input_shape
+        
         self.flatten = nn.Flatten()
-        self.conv1 = nn.Conv2d(1, filter_num, kernel_size = (2,3))
+        self.conv1 = nn.Conv2d(channels, filter_num, kernel_size = (2,3))
         self.conv2 = nn.Sequential(nn.ReLU(),
             nn.MaxPool2d(kernel_size=(1,2), stride = 2),
             nn.Dropout(0.25),
             nn.Conv2d(filter_num, 24, kernel_size = (2,3)),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(1,2), stride = 2),
+            nn.MaxPool2d(kernel_size=(1,2), stride = 2)
         )
 
         self.contain_linar = contain_linear
 
         if contain_linear:
+            
+          row,column = CNN_output_shape(input_dimension=(freq,times))
           self.linear = nn.Sequential(
-              nn.Linear(24*32*14, 1024),
+              nn.Linear(24*row*column, 1024),
               nn.Linear(1024, 5),
           )
 
